6b810cb5ff30bc407940277f46f31f1f28c8632f
Nicolas Olguin <nicoolguin@hotmail.com.ar>
2024-10-01 19:03:40 -0300

    test

--- prestamos/vite.config.ts
+++ prestamos/vite.config.ts
@@ -10,5 +10,5 @@ export default defineConfig({
   server: {
     port: 3000, // Cambia el puerto según sea necesario
   },
-  base: '/',  // Define la ruta base
+  base: '/',  //// Define la ruta base
 })
