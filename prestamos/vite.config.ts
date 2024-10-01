86516e666898d5ed535f52f17d6fcfb1e6a58c11
Nicolas Olguin <nolguin077@gmail.com>
2024-10-01 13:09:46 -0300

    cambios sugeridos para desplegar en vercel

--- prestamos/vite.config.ts
+++ prestamos/vite.config.ts
@@ -4,4 +4,10 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  build: {
+    outDir: 'dist', // Define el directorio de salida
+  },
+  server: {
+    port: 3000, // Cambia el puerto según sea necesario
+  },
 })
