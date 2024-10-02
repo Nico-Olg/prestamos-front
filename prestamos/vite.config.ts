5dd05389bc2bb14e906d1a8ce9f3ac40f11cf3e0
Nicolas Olguin <nicoolguin@hotmail.com.ar>
2024-10-02 07:35:17 -0300

    nueva build

--- prestamos/vite.config.ts
+++ prestamos/vite.config.ts
@@ -1,14 +1,11 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  base: './', // Ruta base apropiada
   build: {
-    outDir: 'dist', // Define el directorio de salida
+    outDir: 'dist',
   },
-  server: {
-    port: 3000, // Cambia el puerto según sea necesario
-  },
-  base: '/',  //// Define la ruta base
-})
+  plugins: [react()],
+});
