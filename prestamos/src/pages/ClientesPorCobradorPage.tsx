b01ceff4b9ca55370f699bf03a04c793cdbf15dd
Nicolas Olguin <nicoolguin@hotmail.com.ar>
2025-04-12 14:08:19 -0300

    feat: Agrega indicador de carga en ClientesPorCobradorPage y estilos correspondientes

--- prestamos/src/pages/ClientesPorCobradorPage.tsx
+++ prestamos/src/pages/ClientesPorCobradorPage.tsx
@@ -10,19 +10,23 @@ const ClientesPorCobradorPage: React.FC = () => {
   const [clientes, setClientes] = useState([]);
   const [cobradorId, setCobradorId] = useState<number | null>(null);
   const [nombreCobrador, setNombreCobrador] = useState<string | null>(null);
+  const [loading, setLoading] = useState<boolean>(false);
   const location = useLocation();
 
   useEffect(() => {
     const fetchClientes = async () => {
       try {
-        const id = location.state?.id; // Ejemplo de ID de cobrador
-       const nombre = location.state?.nombreyApellido || ""; // Obtén el nombre del cobrador del estado
+        setLoading(true); // ⏳ Mostrar loading
+        const id = location.state?.id;
+        const nombre = location.state?.nombreyApellido || "";
         setCobradorId(id);
-        setNombreCobrador(nombre); // Asigna el nombre del cobrador
+        setNombreCobrador(nombre);
         const clientesData = await getClientesPorCobrador(id);
         setClientes(clientesData);
       } catch (error) {
         console.error('Error fetching clientes:', error);
+      } finally {
+        setLoading(false); // ✅ Ocultar loading
       }
     };
 
@@ -31,15 +35,24 @@ const ClientesPorCobradorPage: React.FC = () => {
 
   return (
     <div className="clientes-page">
-      <Header title={`Clientes del Cobrador: ${nombreCobrador  || ''}`}/>
+      <Header title={`Clientes del Cobrador: ${nombreCobrador || ''}`} />
       <div className="content">
         <Sidebar />
-       
-          {cobradorId && (
-          <ClientesPorCobradoresGrid clientes={clientes} cobradorId={cobradorId} nombreCobrador={nombreCobrador || ''}/>
+
+        {loading ? (
+          <div className="loading-container">
+            <div className="spinner" />
+            <p>Cargando clientes...</p>
+          </div>
+        ) : (
+          cobradorId && (
+            <ClientesPorCobradoresGrid
+              clientes={clientes}
+              cobradorId={cobradorId}
+              nombreCobrador={nombreCobrador || ''}
+            />
+          )
         )}
-        
-        
       </div>
     </div>
   );
